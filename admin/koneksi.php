a57edf5bd328cfe82258a3f36173dbd90488407e
m19b <mb19mzz@gmail.com>
2024-12-04 17:55:20 +0800

    coba - coba

--- admin/koneksi.php
+++ admin/koneksi.php
@@ -1,5 +1,14 @@
 <?php
- $kon = mysqli_connect("localhost","root","","tugaskelompok");
+try{
+    $kon = mysqli_connect("localhost","root","","tugaskelompok");
+echo "berhasil";
+}catch(Exception $e){
+    echo "Gagal Konek " . $e->getMessage();
+    exit();
+    header ("location:../index.php");
+}
+
+
 
 
 ?>
