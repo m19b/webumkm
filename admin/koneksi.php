2d2b76bac1d77ad9a05999fe33d97f78be49cc1c
m19b <mb19mzz@gmail.com>
2024-12-05 08:25:29 +0800

    ubah koneksi

--- admin/koneksi.php
+++ admin/koneksi.php
@@ -1,14 +1,11 @@
 <?php
 try{
     $kon = mysqli_connect("localhost","root","","tugaskelompok");
-echo "berhasil";
+
 }catch(Exception $e){
-    echo "Gagal Konek " . $e->getMessage();
+    
+    echo "Gagal Konek : " . $e->getMessage();
     exit();
-    header ("location:../index.php");
+    
 }
-
-
-
-
 ?>
